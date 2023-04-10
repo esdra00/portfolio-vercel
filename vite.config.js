cf5157843179192649765054a04093867936693d
Esdra Bergamasco <esdrabergamasco@gmail.com>
2023-04-10 10:24:37 +0200

    removed base from vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,7 @@
 import react from "@vitejs/plugin-react";
 import {defineConfig} from "vite";
-// import {defineConfig} from "vite";
-
-// const root = resolve(__dirname, "src")
-// const outDir = resolve(__dirname, "dist")
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	// root,
 	plugins: [react()],
-	base: "/portfolioPROD/",
-	// build: {
-	// 	outDir,
-	// 	emptyOutDir: true,
-	// 	rollupOptions: {
-	// 		input: {
-	// 			main: resolve(root, "index.html"),
-	// 			about: resolve(root, "about", "index.html")
-
-	// 		},
-	// 	},
-	// },
 });
